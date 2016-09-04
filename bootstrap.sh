7ac5967c4b45882aa58299388fa4c221d508c8d4
Callum Loh <mail@callumloh.com>
2016-09-04 17:18:17 +1000

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,23 +4,27 @@ echo "Provision VM START"
 echo "=========================================="
 
 sudo apt-get update
+sudo apt-get upgrade
 
 #install nodjs
-sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get -y install nodejs
 
 #install mongo db
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-org
 sudo apt-get update
+
+#Tools
+sudo apt-get install git
 
 #install base npm packages
 sudo npm install -g bower
+sudo npm install -g grunt
 sudo npm install -g grunt-cli
 sudo npm install -g express
 sudo npm install -g yo
